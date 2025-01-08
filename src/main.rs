667803096578ad46ff77cadf30db400a878afb18
Jonas Kell <jonas-kell@web.de>
2025-01-08 07:49:34 +0100

    feat: args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
+use clap::{Parser, ValueEnum};
 use std::net::SocketAddr;
-use tokio::net::UdpSocket;
 use tokio::io;
+use tokio::net::UdpSocket;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Mode of operation: 'server' or 'client'
+    #[arg(value_enum)]
+    mode: Mode,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum Mode {
+    Server,
+    Client,
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let args = Args::parse();
+
     // Listening address and port
     let listen_addr: SocketAddr = "127.0.0.1:8080".parse().expect("Invalid listen address");
-    
+
     // Target address and port
     let target_addr: SocketAddr = "127.0.0.1:9090".parse().expect("Invalid target address");
-    
+
     // Bind a UDP socket to the listening address
     let socket = UdpSocket::bind(listen_addr).await?;
     println!("Listening on {}", listen_addr);
